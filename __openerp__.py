6fed69c174c75b481975847c5bba82a9fad09457
leti12 <leticia.gonzalez@quadit.mx>
2019-09-18 10:41:21 -0500

    ediversa

---
+++ __openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'eDiversa Engine',
+    'version': '9.0.1.0.0',
+    'depends': [
+        'mail',
+        'sale',
+        'purchase'
+    ],
+    'author': 'Quadit, S.A. de C.V.',
+    'description': 'eDiversa',
+    'website': 'https://www.quadit.mx',
+    'data': [
+        'views/ediversa_sale_order.xml',
+	'wizard/albaran_wizard.xml'
+    ],
+    'demo': [],
+    'installable': True
+}
